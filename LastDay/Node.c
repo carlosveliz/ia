1ddada9b86306d4fac8dadfc69738bcff577c399
José Luis Canepa <can@can.im>
2013-09-02 02:37:38 -0400

    Agregada “clase” nodo.

---
+++ LastDay/Node.c
@@ -0,0 +1,74 @@
+//
+//  binpacking.c
+//  BinPacking
+//
+//  Created by José Luis Canepa on 2/9/13.
+//  Copyright (c) 2013 José Luis Canepa. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Node.h"
+
+Node *NodeNew(int w, int h, int x, int y)
+{
+    Node *node = (Node*)malloc(sizeof(Node));
+    
+    node->w = w;
+    node->h = h;
+    node->x = x;
+    node->y = y;
+    
+    node->status = NodeStatusComplete;
+    
+    return node;
+}
+
+Node *NodeNewPartial(int w, int h)
+{
+    Node *node = NodeNew(w, h, 0, 0);
+    
+    node->status = NodeStatusPartial;
+    
+    return node;
+}
+
+void NodeSetBox(Node *node, Box box)
+{
+    node->w = box.w;
+    node->h = box.h;
+    node->x = box.x;
+    node->y = box.y;
+}
+
+void NodeSetLeftChild(Node *node, Node *child)
+{
+    node->left = child;
+    child->parent = node;
+}
+
+void NodeSetRightChild(Node *node, Node *child)
+{
+    node->right = child;
+    child->parent = node;
+}
+
+void NodeFree(Node *node)
+{
+    // Indica al padre que debe indefinir su nodo
+    Node *parent = node->parent;
+    
+    if (parent->left == node)
+    {
+        parent->left = NULL;
+    }
+    else
+    {
+        parent->right = NULL;
+    }
+    
+    node->parent = NULL;
+    
+    // Liberar
+    free(node);
+}
---
+++ LastDay/Node.h
@@ -0,0 +1,54 @@
+//
+//  binpacking.h
+//  BinPacking
+//
+//  Created by José Luis Canepa on 2/9/13.
+//  Copyright (c) 2013 José Luis Canepa. All rights reserved.
+//
+
+#include <stdbool.h>
+
+#ifndef NODE_H
+#define NODE_H
+
+// Estado de un nodo
+typedef enum
+{
+    NodeStatusUndefined,
+    NodeStatusPartial,
+    NodeStatusComplete,
+}NodeStatus;
+
+// Estructura que representa un sistema de coordenadas
+typedef struct
+{
+    int w, h;
+    int x, y;
+}Box;
+
+// Estructura que representa un nodo CBJ, polimorfiza con Box
+typedef struct Node
+{
+    int w, h;
+    int x, y;
+    
+    NodeStatus status;
+    
+    struct Node *parent;
+    struct Node *left;
+    struct Node *right;
+}Node;
+
+// Constructores de Node
+Node *NodeNew(int w, int h, int x, int y);
+Node *NodeNewPartial(int w, int h);
+
+// Setters
+void NodeSetBox(Node *node, Box box);
+void NodeSetLeftChild(Node *node, Node *child);
+void NodeSetRightChild(Node *node, Node *child);
+
+// Destructor
+void NodeFree();
+
+#endif // NODE_H
